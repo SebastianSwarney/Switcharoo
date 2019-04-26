3833d82488d26db751186c4bcf243eb6cecae4bb
TylerTam <46612488+TylerTam@users.noreply.github.com>
2019-04-26 19:13:26 -0400

    Generic AI behaviours
    Generic AI behaviours have been completed
    
    -Shooting enemies added
    -Adjustemnts to melee script so that it works better with the system
    
    -Visual tells restart when recalled now
    
    -Both attack types work with both movement types, and are modular enough to be changed during runtime (i think)

--- Switcharoo/Assets/_Switcharoo/Scripts/AI/AiController.cs
+++ Switcharoo/Assets/_Switcharoo/Scripts/AI/AiController.cs
@@ -2,42 +2,134 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+///<Summary>
+///The brain of the AI. This is essentially an empty shell that requires components to function
 public class AiController : MonoBehaviour
 {
-    public AttackType_Base m_attackType;
-    public MovementType_Base m_movementType;
+
     public GameObject target;
 
+    #region Components on the Enemy
+    public AttackType_Base m_attackType;
+    public MovementType_Base m_movementType;
     Rigidbody2D m_rb;
+    ShootController m_gun;
 
-    [Header ("Attack State")]
+    public Transform m_bulletOrigin;
+    #endregion
+
+    #region Attack Variables
+    [Header("Attack State")]
     public AttackType_Base.AttackState m_currentAttackState = AttackType_Base.AttackState.Finished;
 
     Vector3 m_attackTargetPos;
+
+    [HideInInspector]
+    public Vector3 m_delayedPlayerPosition;
+
+    [HideInInspector]
+    public float m_visualTellTimer;
+    float m_currentShootTimer, m_currentShootDelay;
+
+    bool m_isShooting;
+    #endregion
     void Awake()
     {
         m_rb = GetComponent<Rigidbody2D>();
+        m_gun = GetComponent<ShootController>();
+    }
+    private void Update()
+    {
+        CheckAttackState();
     }
 
-    private void Update() {
-        Debug.DrawLine(transform.position, m_attackTargetPos);
-        if(target != null){
-            //m_movementType.MoveToPosition(m_rb,transform.position,target.transform.position);
-            if(m_currentAttackState == AttackType_Base.AttackState.Finished){
-                print ("Enter Attack state");
-                m_attackType.StartAttack(this,m_rb,target,gameObject);
+    #region Attacking Functions
+    void CheckAttackState()
+    {
+        if (target != null)
+        {
+            //If the attack is finished, and no longer running
+            if (m_currentAttackState == AttackType_Base.AttackState.Finished)
+            {
+                //Restart the attack
+                m_attackType.StartAttack(this, m_rb, target, gameObject, m_gun);
                 m_attackTargetPos = m_attackType.SetAttackTargetPosition(gameObject, target);
-            }else{
-                if(m_attackType.AttackFinished(this,m_rb,m_attackTargetPos,target,gameObject,gameObject.transform)){
-                    print ("Currently Attacking");
-                    target = null;
+            }
+
+            //If the attack is running
+            else
+            {
+
+                //If the attack has ceased
+                if (m_attackType.AttackFinished(this, m_rb, m_attackTargetPos, target, gameObject, m_bulletOrigin, m_gun))
+                {
+                    //If the player is no lnger in the radius, set the target to null
+                    if (!PlayerInRadius())
+                    {
+                        target = null;
+                    }
+                }
+                else
+                {
+                    //If the player has moved a set amount of distance (set in the attack type), recalculate the attackTarget Position
+                    if (PlayerMoved())
+                    {
+                        m_attackTargetPos = m_attackType.SetAttackTargetPosition(gameObject, target);
+                    }
                 }
             }
         }
     }
 
-    /* private void OnDrawGizmos() {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, m_attackType.m_stopAttackDistance);
-    }*/
+    ///<Summary>
+    ///Checks the shooting pattern, to see if the ai can currently shoot
+    public bool CanFireGun()
+    {
+        if (m_currentShootTimer <= m_currentShootDelay)
+        {
+            m_currentShootTimer += Time.deltaTime;
+            return m_isShooting;
+        }
+        else
+        {
+            return !m_isShooting;
+        }
+    }
+
+    ///<Summary>
+    ///Allows the AI to stop shooting, allowing for patterns
+    public void SwapShooting(bool p_isShooting, float p_newDelay)
+    {
+        if (p_isShooting != m_isShooting)
+        {
+            m_isShooting = p_isShooting;
+            m_currentShootDelay = p_newDelay;
+            m_currentShootTimer = 0;
+        }
+
+    }
+
+    ///<Summary>
+    ///If the player has moved a set distance from a spot
+    bool PlayerMoved()
+    {
+        if (Vector3.Distance(target.transform.position, m_delayedPlayerPosition) > m_attackType.m_playerMoveDistanceReaction)
+        {
+            print("Player Moved");
+            m_delayedPlayerPosition = target.transform.position;
+            return true;
+        }
+        return false;
+    }
+
+    ///<Summary>
+    ///Checks if the player is in a set radius
+    ///TODO: Create the following function
+    bool PlayerInRadius()
+    {
+        return false;
+    }
+
+    #endregion
 }
--- Switcharoo/Assets/_Switcharoo/Scripts/AI/Movement/MovementType_Aerial.cs
+++ Switcharoo/Assets/_Switcharoo/Scripts/AI/Movement/MovementType_Aerial.cs
@@ -2,21 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+///<Summary>
+///The aerial movement pattern, used for flying enemeis
+///<Summary>
+
 [CreateAssetMenu(fileName = "Aerial", menuName = "Scriptable Objects/Movement/Aerial", order = 0)]
 public class MovementType_Aerial : MovementType_Base
 {
+
+    ///<Summary>
+    ///Returns the target position in a positon that the enemy can reach
     public override Vector3 ConvertRelativePosition(GameObject p_enemyObject, Vector3 p_convertPos)
     {
         return p_convertPos;
     }
 
+    ///<Summary>
+    ///Moves the enemy to a given position
     public override void MoveToPosition(Rigidbody2D p_rb, Vector3 p_startPos,Vector3 p_targetPosition)
     {
+        ///Removes gravity, to allow for flight
         p_rb.gravityScale = 0;
         Vector3 dir = (p_targetPosition - p_startPos).normalized;
         p_rb.velocity = dir * m_speed;
     }
 
+    ///<Summary>
+    ///Determines if the postion has been reached
     public override bool PostionReached(GameObject p_enemyObject, Vector3 p_targetPos, float p_stoppingDistance)
     {
         if(Vector3.Distance(p_targetPos, p_enemyObject.transform.position) <= p_stoppingDistance){
