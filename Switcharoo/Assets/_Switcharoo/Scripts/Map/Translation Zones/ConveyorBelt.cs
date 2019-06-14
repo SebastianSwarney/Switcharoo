c1e81e1bbadd90b8238177334036024d145bbd9c
SebastianSwarney <34040389+SebastianSwarney@users.noreply.github.com>
2019-06-14 15:52:54 -0400

    Added layer masks to the translation zones

--- Switcharoo/Assets/_Switcharoo/Scripts/Map/Translation Zones/ConveyorBelt.cs
+++ Switcharoo/Assets/_Switcharoo/Scripts/Map/Translation Zones/ConveyorBelt.cs
@@ -2,13 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConveyorBelt : MonoBehaviour, IActivatable, IPauseable
+public class ConveyorBelt : TranslationZone_Base, IActivatable, IPauseable
 {
-    /*
-	public enum MoveDirection { Left, Right, Up, Down }
-	public MoveDirection m_moveDirection;
-	*/
-
     public float m_moveSpeed;
 
     [Header("Active Properties")]
@@ -22,44 +17,19 @@ public class ConveyorBelt : MonoBehaviour, IActivatable, IPauseable
         ObjectPooler.instance.AddObjectToPooler(gameObject);
         m_isActive = m_startActive;
     }
+
     private void OnCollisionStay2D(Collision2D collision)
     {
 
         if (m_isPaused) return;
         if (m_isActive)
         {
-
-
-            PlayerController player = collision.gameObject.GetComponent<PlayerController>();
-
-            player.m_velocity += transform.right * m_moveSpeed;
-
+			if (CheckCollisionLayer(m_playerMask, collision.collider))
+			{
+				PlayerController player = collision.gameObject.GetComponent<PlayerController>();
+				player.m_velocity += transform.right * m_moveSpeed;
+			}
         }
-        /*
-		switch (m_moveDirection)
-		{
-			case MoveDirection.Left:
-
-				player.m_velocity += Vector3.left * m_moveSpeed;
-
-				break;
-			case MoveDirection.Right:
-
-				player.m_velocity += Vector3.right * m_moveSpeed;
-
-				break;
-			case MoveDirection.Up:
-
-				player.m_velocity += Vector3.up * m_moveSpeed;
-
-				break;
-			case MoveDirection.Down:
-
-				player.m_velocity += Vector3.down * m_moveSpeed;
-
-				break;
-		}
-		*/
     }
 
     #region IActivatable
--- Switcharoo/Assets/_Switcharoo/Scripts/Map/Translation Zones/ZeroGravityZone.cs
+++ Switcharoo/Assets/_Switcharoo/Scripts/Map/Translation Zones/ZeroGravityZone.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ZeroGravityZone : MonoBehaviour
+public class ZeroGravityZone : TranslationZone_Base
 {
 	private void OnCollisionEnter2D(Collision2D collision)
 	{
-		collision.gameObject.GetComponent<PlayerController>().m_states.m_gravityControllState = PlayerController.GravityState.GravityDisabled;
+		if (CheckCollisionLayer(m_playerMask, collision.collider))
+		{
+			collision.gameObject.GetComponent<PlayerController>().m_states.m_gravityControllState = PlayerController.GravityState.GravityDisabled;
+		}
 	}
 
 	private void OnCollisionExit2D(Collision2D collision)
 	{
-		collision.gameObject.GetComponent<PlayerController>().m_states.m_gravityControllState = PlayerController.GravityState.GravityEnabled;
+		if (CheckCollisionLayer(m_playerMask, collision.collider))
+		{
+			collision.gameObject.GetComponent<PlayerController>().m_states.m_gravityControllState = PlayerController.GravityState.GravityEnabled;
+		}
 	}
 }
