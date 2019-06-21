13011170e1067a44e49b727533b3503f90cec16b
TylerTam <46612488+TylerTam@users.noreply.github.com>
2019-06-21 16:59:35 -0400

    Menu Systems
    Most menu systems have been created
    -Awaiting Controller integration with the buttons
    
    -Scene transitions have been made

--- Switcharoo/Assets/_Switcharoo/Scripts/Interface/SceneTransition/LoadNewScene.cs
+++ Switcharoo/Assets/_Switcharoo/Scripts/Interface/SceneTransition/LoadNewScene.cs
@@ -6,4 +6,13 @@ public class LoadNewScene : MonoBehaviour
     {
         SceneManager.LoadScene(p_newScene);
     }
+    public void LoadScene(int p_sceneIndex)
+    {
+        SceneManager.LoadScene(p_sceneIndex);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
