1b767d2cf93c17e23193cf2cebbc0ae8d9ce5acf
SebastianSwarney <34040389+SebastianSwarney@users.noreply.github.com>
2019-06-24 12:48:22 -0400

    Timer hazard start delay and trigger hazard fix

--- Switcharoo/Assets/_Switcharoo/Scripts/Map/Hazards/CollisionHazard_Timer.cs
+++ Switcharoo/Assets/_Switcharoo/Scripts/Map/Hazards/CollisionHazard_Timer.cs
@@ -7,9 +7,18 @@ public class CollisionHazard_Timer : CollisionHazard_Base
 	[Header("Timer Hazard Properties")]
 	public float m_timeOn;
 	public float m_timeOff;
+	public float m_startDelay;
 
 	private bool m_isOn;
 	private float m_timer;
+	private bool m_isStarted;
+
+	public override void Start()
+	{
+		base.Start();
+
+		StartCoroutine(StartDelay());
+	}
 
 	private void Update()
 	{
@@ -17,35 +26,62 @@ public class CollisionHazard_Timer : CollisionHazard_Base
         HazardTimer();
 	}
 
+	IEnumerator StartDelay()
+	{
+		m_isStarted = false;
+		float t = 0f;
+
+		while (t < m_startDelay)
+		{
+			t += Time.deltaTime;
+
+			yield return null;
+		}
+		m_isStarted = true;
+	}
+
 	private void HazardTimer()
 	{
-		if (m_isOn)
+		if (m_isStarted)
 		{
-			m_renderer.color = Color.white;
-			m_damageTimer = m_damageInterval;
-			m_canDamage = true;
-
-			m_timer += Time.deltaTime;
-
-			if (m_timer >= m_timeOn)
+			if (m_isOn)
 			{
-				m_timer = 0f;
-				m_isOn = false;
+				TurnOn();
+
+				m_damageTimer = m_damageInterval;
+				m_timer += Time.deltaTime;
+
+				if (m_timer >= m_timeOn)
+				{
+					m_timer = 0f;
+					m_isOn = false;
+				}
+			}
+
+			if (!m_isOn)
+			{
+				TurnOff();
+
+				m_timer += Time.deltaTime;
+
+				if (m_timer >= m_timeOff)
+				{
+					m_timer = 0f;
+					m_isOn = true;
+				}
 			}
 		}
+	}
 
-		if (!m_isOn)
-		{
-			m_renderer.color = Color.clear;
-			m_canDamage = false;
+	private void TurnOff()
+	{
+		m_renderer.color = Color.clear;
+		m_canDamage = false;
+	}
 
-			m_timer += Time.deltaTime;
-
-			if (m_timer >= m_timeOff)
-			{
-				m_timer = 0f;
-				m_isOn = true;
-			}
-		}
+	private void TurnOn()
+	{
+		m_renderer.color = Color.white;
+		m_canDamage = true;
 	}
 }
