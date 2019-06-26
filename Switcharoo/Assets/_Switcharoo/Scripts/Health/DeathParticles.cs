06340f54bf2ad3d6934f734a6ddc380510d3bd78
MSI\Wu <zhangwu@sheridancollege.ca>
2019-06-26 00:20:08 -0400

    Wu Death Particles

---
+++ Switcharoo/Assets/_Switcharoo/Scripts/Health/DeathParticles.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathParticles : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        this.transform.position += transform.up * speed / 10f;
+    }
+}
