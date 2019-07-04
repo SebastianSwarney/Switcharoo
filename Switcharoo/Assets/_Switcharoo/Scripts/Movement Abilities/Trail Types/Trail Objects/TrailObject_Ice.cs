06b661b80df00ba4f6968bac2618c74a4626d42c
TylerTam <46612488+TylerTam@users.noreply.github.com>
2019-07-04 13:02:03 -0400

    More trail type sounds implemented

--- Switcharoo/Assets/_Switcharoo/Scripts/Movement Abilities/Trail Types/Trail Objects/TrailObject_Ice.cs
+++ Switcharoo/Assets/_Switcharoo/Scripts/Movement Abilities/Trail Types/Trail Objects/TrailObject_Ice.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class TrailObject_Ice : TrailObject_Base
 {
+    public OnActivationEvent m_iceLandSound;
+    public GameObject m_iceParticleBurst;
 	[HideInInspector]
 	public TrailType_Ice m_trailType;
 	[HideInInspector]
 	public LayerMask m_damageTargetMask;
-    private void OnEnable()
+    ObjectPooler m_pooler;
+    
+    private void Start()
     {
-        ObjectPooler.instance.AddObjectToDespawn(this.gameObject);
+        m_pooler = ObjectPooler.instance;
+        m_pooler.AddObjectToDespawn(this.gameObject);
+
     }
     private void OnCollisionEnter2D(Collision2D collision)
 	{
+        m_iceLandSound.Invoke();
+        m_pooler.NewObject(m_iceParticleBurst, transform.position, Quaternion.identity);
 		m_trailType.IceBlast(transform.position, m_damageTargetMask);
 		ObjectPooler.instance.ReturnToPool(gameObject);
 	}
