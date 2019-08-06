f8da82770265f1651ad4bfdf78fb40874424ca0c
SebastianSwarney <34040389+SebastianSwarney@users.noreply.github.com>
2019-08-06 11:02:32 -0400

    Volume Sliders

--- Switcharoo/Assets/_Switcharoo/Scripts/Sound/SoundManager.cs
+++ Switcharoo/Assets/_Switcharoo/Scripts/Sound/SoundManager.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using FMOD.Studio;
 using FMODUnity;
+using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour
 {
 	public static SoundManager instance;
 
-	[Range(0f, 1f)]
-	public float m_masterVolume = 1f;
+	public Slider m_masterSlider;
+	public Slider m_musicSlider;
+	public Slider m_sfxSlider;
 
 	private Bus m_masterBus;
+	private Bus m_musicBus;
+	private Bus m_sfxBus;
 
 	private void Awake()
 	{
@@ -24,13 +28,20 @@ public class SoundManager : MonoBehaviour
 			Destroy(this);
 		}
 
-		//m_masterBus = RuntimeManager.GetBus("bus:/Master");
+		m_masterBus = RuntimeManager.GetBus("bus:/Master");
+		m_musicBus = RuntimeManager.GetBus("bus:/Master/Music");
+		m_sfxBus = RuntimeManager.GetBus("bus:/Master/SFX");
+	}
+
+	private void Update()
+	{
+		SetBusVolume(m_masterBus, m_masterSlider.value);
+		SetBusVolume(m_musicBus, m_musicSlider.value);
+		SetBusVolume(m_sfxBus, m_sfxSlider.value);
 	}
 
 	public void SetBusVolume(Bus p_bus, float p_volume)
 	{
 		p_bus.setVolume(p_volume);
 	}
-
-
 }
